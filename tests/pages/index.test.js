1a1233c4ff62d12b85a1a0a2e20785bb0e2efb3e
Luis Valencia <luis@zitus.mx>
2022-07-14 18:48:59 -0500

    testing update

--- tests/pages/index.test.js
+++ tests/pages/index.test.js
@@ -1,6 +1,9 @@
 import '@testing-library/jest-dom'
 import { render, screen, fireEvent } from '@testing-library/react'
-import Index from '../pages/index'
+import { isExportDeclaration } from 'typescript'
+// import Index from '../pages/index'
+
+import RegisterPage from '../pages/index'
 
 const formInputValues = [
     {
@@ -31,7 +34,44 @@ describe('Simple working form', () => {
     // It takes two arguments: a string describing the test suit and a callback function for wrapping the test functionality
     it('Should render all form inputs', () => {
         render(
-            
+            <RegisterPage />
         )
+
+        // check for all form fields 
+        formInputValues.forEach((value, index) => {
+            expect(screen.getByLabelText(value.label)).toBeInTheDocument()
+        })
+    })
+
+    it('Should render submit button', async () => {
+        render(
+            <RegisterPage />
+        )
+
+        const button = screen.getByRole('button', {name: 'Create account'})
+
+        expect(button).toBeInTheDocument()
+
+        expect(button).not.toBeDisabled()
+    })
+
+    it('Should submit when inputs are filled and submit button clickd', async() => {
+        render(
+            <RegisterPage />
+        )
+
+        const submitButton = screen.getByRole('button', {name: 'Create account'})
+
+        formInputValues.forEach((mockValue, index) => {
+            const input = screen.getByLabelText(mockValue.label)
+            fireEvent.change(input, { target: {value: mockValue.correctTestValue}})
+        })
+
+        fireEvent.click(submitButton)
+
+        expect(
+            await screen.findByRole('button', {name: 'Loading...'})
+        ).toBeInTheDocument()
     })
 })
+
